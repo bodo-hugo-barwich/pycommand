a7260954e66f7e48f1a5794f9854aa3f15ef875d
Bodo Hugo Barwich <b.barwich@hotmail.com>
2020-07-11 22:55:24 +0100

    Command Class

---
+++ libcommand/libcommand.py
@@ -0,0 +1,14 @@
+'''
+This Module provides the `Command` Class which launches a Single Child Process
+in asynchronous Mode and captures possible Errors.
+
+:version: 2020-07-11
+
+:author: Bodo Hugo Barwich
+'''
+__docformat__ = "restructuredtext en"
+
+
+
+class Command(object):
+  pass
